4b46929fed11427fbdc752aa28a7db48694ff8ae
naanati <126883847+naanati@users.noreply.github.com>
2023-07-17 17:50:49 -0300

    aula-dom-eventos-javascript.
    exercícios em js

---
+++ script.js
@@ -0,0 +1,8 @@
+function minhaFuncao (){
+
+var elementos = document. getElementsByTagName ("p") ;
+
+for(let i = 0; i < elementos.length; i++){
+elementos [i].innerHTML = "Olá, Seja bem vindo ao mundo JS!";
+}
+}
